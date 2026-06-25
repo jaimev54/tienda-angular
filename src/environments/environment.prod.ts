ce6d75d530df3274cf68027181cc9598f47715ac
jaime_v54 <jaimevargaa5@gmail.com>
2026-06-25 16:07:07 -0600

    Agregar URL de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,5 @@
-﻿export const environment = { production: true, apiUrl: 'https://tu-api.onrender.com/api' };
+﻿export const environment = {
+  production: true,
+  apiUrl: 'https://tienda-api-l96l.onrender.com/api',
+  stripePublicKey: 'pk_test_51TihCT2ZiJRg3geWJxczJRQVFGvVcciYxi9ho3b8z7pwxhc6UMQq43iTgzWXhOW8Bo6mMkHbeLEqAZREWvmaGxpF00XHzJfYYn'
+};
